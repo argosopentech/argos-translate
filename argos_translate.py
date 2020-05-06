65d6f9ece4f2b926aaae1dacf04e270a43fa3fb5
Argos Open Technologies, LLC <admin@argosopentech.com>
2020-05-06 12:38:20 -0400

    Got app starting inside snap container

--- argos_translate.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from gui import GUI
-
-if __name__ == "__main__":
-    GUI.main_loop()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Argos Translate',
+    version='1.0',
+    description='Offline translation app',
+    author='Argos Open Technologies, LLC',
+    author_email='admin@argosopentech.com',
+    url='https://www.argosopentech.com',
+    packages=['argos_translate'],
+    scripts=['bin/argos-translate']
+)
