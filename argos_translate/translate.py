89b974807deefd32ae8875e900536a409a5732f2
Argos Open Technologies, LLC <admin@argosopentech.com>
2020-05-09 12:44:21 -0400

    Added ability to pivot through languages

--- argos_translate/translate.py
+++ argos_translate/translate.py
@@ -14,7 +14,6 @@ class Language:
         return self.name
 
     def get_translation(self, to):
-        print(list(map(str, self.translations_from)))
         valid_translations = list(filter(lambda x: x.to_lang.code == to.code, self.translations_from))
         if len(valid_translations) > 0:
             return valid_translations[0]
@@ -46,3 +45,24 @@ en_es = Translation(en, es, apertium_translation(en, es))
 es_en = Translation(es, en, apertium_translation(es, en))
 en_eo = Translation(en, eo, apertium_translation(en, eo))
 eo_en = Translation(eo, en, apertium_translation(eo, en))
+
+# Everything can translate to itself
+for language in languages:
+    Translation(language, language, lambda x: x)
+
+# Pivot through other languages to add translations
+def composite_fun(first, second):
+    return lambda x: second(first(x))
+
+for language in languages:
+    keep_adding_translations = True
+    while keep_adding_translations:
+        keep_adding_translations = False
+        for translation in language.translations_from:
+            for translation_2 in translation.to_lang.translations_from:
+                if language.get_translation(translation_2.to_lang) == None:
+                    # The language currently doesn't have a way to translate to this language
+                    keep_adding_translations = True
+                    trans_fun = composite_fun(translation.translate_function, translation_2.translate_function)
+                    Translation(language, translation_2.to_lang, trans_fun) 
+                
