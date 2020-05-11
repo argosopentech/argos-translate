a5a4093b66f45cc9271959181b6b5ab3d5990f5f
Argos Open Technologies, LLC <admin@argosopentech.com>
2020-05-11 14:32:36 -0400

    Added Catalan support

--- argos_translate/translate.py
+++ argos_translate/translate.py
@@ -30,21 +30,25 @@ class Translation:
     def __str__(self):
         return str(self.from_lang) + ' -> ' + str(self.to_lang)
 
-def apertium_translation(from_lang, to_lang):
+def apertium_translation(from_code, to_code):
     def to_return(input_text):
-        return os.popen('echo \'' + input_text + '\' | apertium ' + from_lang.code + '-' + to_lang.code + '').read()
+        return os.popen('echo \'' + input_text + '\' | apertium ' + from_code + '-' + to_code + '').read()
     return to_return
 
 # Languages
 en = Language('en', 'English')
 es = Language('es', 'Spanish')
 eo = Language('eo', 'Esperanto')
+ca = Language('ca', 'Catalan')
 
 # Translations
-en_es = Translation(en, es, apertium_translation(en, es))
-es_en = Translation(es, en, apertium_translation(es, en))
-en_eo = Translation(en, eo, apertium_translation(en, eo))
-eo_en = Translation(eo, en, apertium_translation(eo, en))
+en_es = Translation(en, es, apertium_translation('en', 'es'))
+es_en = Translation(es, en, apertium_translation('es', 'en'))
+en_eo = Translation(en, eo, apertium_translation('en', 'eo'))
+eo_en = Translation(eo, en, apertium_translation('eo', 'en'))
+en_ca = Translation(en, ca, apertium_translation('en', 'ca'))
+ca_en = Translation(ca, en, apertium_translation('ca', 'en'))
+
 
 # Everything can translate to itself
 for language in languages:
