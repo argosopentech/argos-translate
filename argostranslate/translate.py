05ef29fe4aa00fcdd1fca2efafd42a3a76d9dba2
Argos Open Technologies, LLC <admin@argosopentech.com>
2020-09-12 10:39:26 -0500

    Fixed bug in translating with multiple packages installed

--- argostranslate/translate.py
+++ argostranslate/translate.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from functools import partial
 
 import ctranslate2
 import sentencepiece as spm
@@ -51,20 +52,48 @@ class Translation:
     Attributes:
         from_lang (Language): The Language this Translation translates from.
         to_lang (Language): The Language this Translation translates to.
-        translate_function (str -> str): A function that translates a str
-            from from_lang to to_lang.
+        pkg (Package): The installed package that provides this translation.
 
     """
 
-    def __init__(self, from_lang, to_lang, translate_function):
+    def __init__(self, from_lang, to_lang, pkg):
         self.from_lang = from_lang
         self.to_lang = to_lang
-        self.translate_function = translate_function
+        self.pkg = pkg
+        self.translate_function = partial(translate_function, self.pkg)
         from_lang.translations_from.append(self)
         to_lang.translations_to.append(self)
 
     def __str__(self):
         return str(self.from_lang) + ' -> ' + str(self.to_lang)
+
+def translate_function(pkg, input_text): 
+    """Uses the translation in pkg to translate input_text.
+
+    Args:
+        pkg (Package): The package that provides the translation.
+        input_text (str): The text to be translated.
+
+    """
+
+    model_path = str(pkg.package_path / 'model')
+    translator = ctranslate2.Translator(model_path)
+    sp_model_path = str(pkg.package_path / 'sentencepiece.model')
+    sp_processor = spm.SentencePieceProcessor(model_file=sp_model_path)
+    stanza_pipeline = stanza.Pipeline(lang=pkg.from_code,
+            dir=str(pkg.package_path / 'stanza'),
+            processors='tokenize', use_gpu=False)
+    tokenized = stanza_pipeline(input_text)
+    sentences = [sentence.text for sentence in tokenized.sentences]
+    to_return = ''
+    for sentence in sentences:
+        tokenized = sp_processor.encode(sentence, out_type=str)
+        translated = translator.translate_batch([tokenized])
+        translated = translated[0][0]['tokens']
+        detokenized = ''.join(translated)
+        detokenized = detokenized.replace('▁', ' ')
+        to_return += detokenized
+    return to_return.strip()
 
 def load_installed_languages():
     """Returns a list of Languages installed from packages"""
@@ -72,6 +101,7 @@ def load_installed_languages():
     packages = package.get_installed_packages()
     language_of_code = dict()
     for pkg in packages:
+        print(pkg.package_path)
         if pkg.from_code not in language_of_code:
             language_of_code[pkg.from_code] = Language(
                     pkg.from_code, pkg.from_name)
@@ -79,31 +109,10 @@ def load_installed_languages():
             language_of_code[pkg.to_code] = Language(
                     pkg.to_code, pkg.to_name)
 
-        def translate_function(input_text):
-            model_path = str(pkg.package_path / 'model')
-            translator = ctranslate2.Translator(model_path)
-            sp_model_path = str(pkg.package_path / 'sentencepiece.model')
-            sp_processor = spm.SentencePieceProcessor(model_file=sp_model_path)
-            stanza_pipeline = stanza.Pipeline(lang=pkg.from_code,
-                    dir=str(pkg.package_path / 'stanza'),
-                    processors='tokenize', use_gpu=False)
-            tokenized = stanza_pipeline(input_text)
-            sentences = [sentence.text for sentence in tokenized.sentences]
-            to_return = ''
-            for sentence in sentences:
-                tokenized = sp_processor.encode(sentence, out_type=str)
-                translated = translator.translate_batch([tokenized])
-                translated = translated[0][0]['tokens']
-                detokenized = ''.join(translated)
-                detokenized = detokenized.replace('▁', ' ')
-                to_return += detokenized
-            return to_return.strip()
-
         Translation(language_of_code[pkg.from_code],
-                language_of_code[pkg.to_code], translate_function)
+                language_of_code[pkg.to_code], pkg)
 
     languages = list(language_of_code.values())
-
     # Everything can translate to itself
     for language in languages:
         Translation(language, language, lambda x: x)
@@ -123,8 +132,7 @@ def load_installed_languages():
                         keep_adding_translations = True
                         trans_fun = composite_fun(translation.translate_function,
                                 translation_2.translate_function)
-                        Translation(language, translation_2.to_lang, trans_fun) 
-    
+                        Translation(language, translation_2.to_lang, trans_fun)  
     languages.sort(key=lambda x: x.code)
     return languages
 
