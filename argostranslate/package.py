5cb02eb6a2b6e4dc377b6d47d2da806149c3c8b6
Argos Open Technologies, LLC <admin@argosopentech.com>
2020-09-12 20:13:30 -0500

    Made it easier to add other data

--- argostranslate/package.py
+++ argostranslate/package.py
@@ -60,8 +60,8 @@ class Package:
             self.to_code = metadata.get('to_code')
             self.to_name = metadata.get('to_name')
 
-def check_data_dir():
-    """Checks that data dir is set up correctly.
+def check_data_dirs():
+    """Checks that data and package dir is set up correctly.
 
     Checks that the data directory in settings.py exist
     and creates it if it doesn't
@@ -69,6 +69,8 @@ def check_data_dir():
     """
     if not os.path.exists(settings.data_dir):
         os.makedirs(settings.data_dir)
+    if not os.path.exists(settings.package_data_dir):
+        os.makedirs(settings.package_data_dir)
 
 def install_from_path(path):
     """Install a package (zip archive ending in .argosmodel).
@@ -79,9 +81,9 @@ def install_from_path(path):
     """
     if not zipfile.is_zipfile(path):
         raise Error('Not a valid Argos Model (must be a zip archive)')
-    check_data_dir()
+    check_data_dirs()
     with zipfile.ZipFile(path, 'r') as zip:
-        zip.extractall(path=settings.data_dir)
+        zip.extractall(path=settings.package_data_dir)
 
 def uninstall(pkg):
     """Uninstalls a package.
@@ -99,7 +101,7 @@ def get_installed_packages(path=None):
             Defaults to the path in settings module.
 
     """
-    check_data_dir()
+    check_data_dirs()
     to_return = []
     packages_path = settings.package_dirs if path == None else path
     for directory in packages_path:
--- argostranslate/settings.py
+++ argostranslate/settings.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import os
 
 data_dir = Path.home() / '.argos-translate'
+package_data_dir = data_dir / 'packages'
 
 # Will search all of these directories for packages
-package_dirs = [data_dir]
+package_dirs = [package_data_dir]
 if 'SNAP' in os.environ:
     package_dirs.append(
             Path(os.environ['SNAP']) / 'packages')
