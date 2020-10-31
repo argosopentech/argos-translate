0765e10e4eca01da979ed6c31b2175429bab681b
Argos Open Technologies, LLC <admin@argosopentech.com>
2020-10-31 11:34:03 -0500

    Documented snap upload process

---
+++ scripts/build_and_upload_snap.sh
@@ -0,0 +1,13 @@
+exit 1
+# This script isn't currently runnable it just documents the process
+# Remember to update version number
+# Remember to just include the French unzipped packages
+
+cd ..
+snapcraft clean && SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=4G snapcraft
+
+# Install and test snap
+
+snapcraft login
+snapcraft upload --release=stable mysnap_latest_amd64.snap
+
