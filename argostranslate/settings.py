e38550eeffeed7c6768748319461aa2992b23e37
redox <mike.moritz@vertex.link>
2020-12-31 11:03:50 -0800

    check snap package dir before adding/iterating

--- argostranslate/settings.py
+++ argostranslate/settings.py
@@ -10,13 +10,17 @@ package_data_dir = data_dir / 'packages'
 package_dirs = [package_data_dir]
 if 'SNAP' in os.environ:
     # Packages bundled with snap
-    package_dirs.append(Path(os.environ['SNAP']) / 'snap_custom' / 'packages')
+    snap_package_dir = Path(os.environ['SNAP']) / 'snap_custom' / 'packages'
+    if os.path.isdir(snap_package_dir):
+        package_dirs.append(snap_package_dir)
 
     # Packages loaded from a content snap
     content_snap_packages = Path(os.environ['SNAP']) / 'snap_custom' / 'content_snap_packages'
-    for package_dir in content_snap_packages.iterdir():
-        if package_dir.is_dir():
-            package_dirs.append(package_dir)
+    if os.path.isdir(content_snap_packages):
+        for package_dir in content_snap_packages.iterdir():
+            if package_dir.is_dir():
+                package_dirs.append(package_dir)
+
 if 'ARGOS_TRANSLATE_PACKAGE_DIR' in os.environ:
     package_dirs.append(Path(os.environ[
             'ARGOS_TRANSLATE_PACKAGE_DIR']))
