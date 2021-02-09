1ae5bc9aeba89172496a09367e7e3a3c471dc5a3
P.J. Finlay <pjappdevelopment@gmail.com>
2021-02-08 20:33:04 -0600

    Made version 1.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='argostranslate',
-    version='1.1.1',
+    version='1.1.2',
     description='Offline neural machine translation library and GUI.',
     long_description=long_description,
     long_description_content_type='text/markdown',
