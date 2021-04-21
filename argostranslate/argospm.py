6e0292d3999d050d8f1b38cc963b287ca1dda638
Alain Leufroy <none@none>
2021-04-22 01:04:41 +0200

    Extract function in argos package manager CLI

--- argostranslate/argospm.py
+++ argostranslate/argospm.py
@@ -17,7 +17,52 @@ def name_of_package(pkg):
             (package.IPackage) Package to get name of.
     """
     return 'translate-' + pkg.from_code + '_' + pkg.to_code
-    
+
+
+def update_index(args):
+    """Update the package index."""
+    package.update_package_index()
+
+
+def install_package(args):
+    """Install package."""
+    available_packages = package.get_available_packages()
+    package_name = args.argument
+    package_found = False
+    for available_package in available_packages:
+        name = name_of_package(available_package)
+        if name == package_name:
+            download_path = available_package.download()
+            package.install_from_path(download_path)
+            print(f'Installed package to path {download_path}')
+            package_found = True
+            break
+    if not package_found:
+        print('Package not found')
+
+
+def list_packages(args):
+    """List packages."""
+    installed_packages = package.get_installed_packages()
+    for installed_package in installed_packages:
+        print(name_of_package(installed_package))
+
+
+def remove_package(args):
+    """Remove installed package."""
+    installed_packages = package.get_installed_packages()
+    package_name = args.argument
+    package_found = False
+    for installed_package in installed_packages:
+        name = name_of_package(installed_package)
+        if name == package_name:
+            installed_package.remove()
+            print(f'Removed package {name}')
+            package_found = True
+            break
+    if not package_found:
+        print('Package not found')
+
 
 def main():
     commands = ['update', 'install', 'list', 'remove']
@@ -36,38 +81,13 @@ def main():
         exit(1)
 
     if command == 'update':
-        package.update_package_index()
+        update_index(args)
 
     elif command == 'install':
-        available_packages = package.get_available_packages()
-        package_name = args.argument
-        package_found = False
-        for available_package in available_packages:
-            name = name_of_package(available_package)
-            if name == package_name:
-                download_path = available_package.download()
-                package.install_from_path(download_path)
-                print(f'Installed package to path {download_path}')
-                package_found = True
-                break
-        if not package_found:
-            print('Package not found')
+        install_package(args)
 
     elif command == 'list':
-        installed_packages = package.get_installed_packages()
-        for installed_package in installed_packages:
-            print(name_of_package(installed_package))
+        list_packages(args)
 
     elif command == 'remove':
-        installed_packages = package.get_installed_packages()
-        package_name = args.argument
-        package_found = False
-        for installed_package in installed_packages:
-            name = name_of_package(installed_package)
-            if name == package_name:
-                installed_package.remove()
-                print(f'Removed package {name}')
-                package_found = True
-                break
-        if not package_found:
-            print('Package not found')
+        remove_package(args)
