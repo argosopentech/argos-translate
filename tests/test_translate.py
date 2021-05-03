a9f62d9b32fe1828e6ab8493ee02184433d4ebf2
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-03 15:18:03 -0700

    Add init, TestITranslation and repr for Hypothesis

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,3 +1,4 @@
+import pytest
 from argostranslate import translate
 
 
@@ -19,4 +20,35 @@ class TestHypothesis:
             {"input": ("a word", 34.0), "output": "('a word', 34.0)"},
         ]
         for test_case in test_cases:
-            assert str(test_case["input"]) == test_case["output"]
+            string = str(translate.Hypothesis(*test_case["input"]))
+            assert string == test_case["output"]
+
+    def test_repr(self):
+        test_cases = [
+            {"input": ("this", 0.2), "output": "Hypothesis('this', 0.2)"},
+            {"input": ("another thing", 3.0), "output": "Hypothesis('another thing', 3.0)"},
+        ]
+        for test_case in test_cases:
+            current_repr = translate.Hypothesis(*test_case["input"]).__repr__()
+            assert current_repr == test_case["output"]
+
+
+class TestITranslation:
+    def test_translate(self):
+        with pytest.raises(NotImplementedError):
+            translate.ITranslation().translate("some input")
+
+    def test_hypotheses(self):
+        with pytest.raises(NotImplementedError):
+            translate.ITranslation().hypotheses("this is some text")
+
+    def test_split_into_paragraphs(self):
+        test_cases = [
+            {"input": "first\nand second line", "output": ["first", "and second line"]},
+            {"input": "this is\n on two lines", "output": ["this is", " on two lines"]},
+        ]
+        for test_case in test_cases:
+            assert (
+                translate.ITranslation.split_into_paragraphs(test_case["input"])
+                == test_case["output"]
+            )
