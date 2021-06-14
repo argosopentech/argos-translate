3647535d73e9db8e106bb61fa651b50a62c426fb
P.J. Finlay <pjappdevelopment@gmail.com>
2021-06-14 06:59:23 -0500

    Improved LIbreTranslate API connection

--- argostranslate/apis.py
+++ argostranslate/apis.py
@@ -2,23 +2,27 @@ import json
 import sys
 from urllib import request, parse
 
-from argostranslate import models
-from argostranslate.models import ILanguageModel
+from argostranslate.models import ILanguageModel, ITranslation
 
 
-class LibreTranslateTranslation(ITranslation):
-    def api_connect(
-        q, source="en", target="es", url="https://translate.astian.org/translate"
-    ):
+class LibreTranslateAPI:
+    DEFAULT_URL = "https://translate.astian.org/"
+    # TODO: Add constructor to set url
+
+    def translate(self, q, source="en", target="es", url=None):
         """Connect to LibreTranslate API
 
         Args:
             q (str): The text to translate
             source (str): The source language code (ISO 639)
             target (str): The target language code (ISO 639)
+            url (str): The url for the translate endpoint. None for default.
 
         Returns: The translated text
         """
+        if url is None:
+            url = LibreTranslateAPI.DEFAULT_URL + "translate"
+
         params = {"q": q, "source": source, "target": target}
 
         url_params = parse.urlencode(params)
@@ -39,9 +43,50 @@ class LibreTranslateTranslation(ITranslation):
 
         return json.loads(response_str)
 
-    def hypotheses(self, input_text, num_hypotheses):
-        assert num_hypotheses == 1
-        return api_connect(input_text)["translatedText"]
+    def languages(url=None):
+        """Connect to LibreTranslate API
+
+        Args:
+            url (str): The url for the languages endpoint. None for default.
+
+        Returns: The translated text
+        """
+
+        if url is None:
+            url = LibreTranslateAPI.DEFAULT_URL + "languages"
+
+        req = request.Request(url, method="POST")
+
+        try:
+            response = request.urlopen(req)
+        except Exception as e:
+            print(e, sys.stderr)
+            return None
+
+        try:
+            response_str = response.read().decode()
+        except Exception as e:
+            print(e, sys.stderr)
+            return None
+
+        return json.loads(response_str)
+
+
+class LibreTranslateTranslation(ITranslation):
+    def __init__(self, from_lang, to_lang, api):
+        self.from_lang = from_lang
+        self.to_lang = to_lang
+        self.api = api
+
+    def hypotheses(self, input_text, num_hypotheses=1):
+        """LibreTranslate only supports single hypotheses.
+
+        A list of length num_hypotheses will be returned with identical hypotheses.
+        """
+        result = self.api.translate(input_text, self.from_lang, self.to_lang)[
+            "translatedText"
+        ]
+        return [result] * num_hypotheses
 
 
 # OpenAI API
