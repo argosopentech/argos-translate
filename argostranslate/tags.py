56db272d775794d7fac2c0ae547100a899dc9067
P.J. Finlay <pjappdevelopment@gmail.com>
2021-07-24 12:07:04 -0500

    Inference tag injection working

--- argostranslate/tags.py
+++ argostranslate/tags.py
@@ -1,4 +1,22 @@
 from argostranslate import translate
+from argostranslate.utils import info, error
+
+"""
+import argostranslate
+from argostranslate.tags import *
+from argostranslate import package, translate
+
+installed_languages = translate.get_installed_languages()
+translation_en_es = installed_languages[0].get_translation(installed_languages[1])
+
+tag_translation = TagTranslation(translation_en_es)
+t = Tag(['I went to ', TagLiteral('Paris'), ' last summer.'])
+
+translated_tags = tag_translation.translate_tag(t).children()
+for translated_tag in translated_tags:
+    print(str(translated_tag))
+
+"""
 
 
 class ITag:
@@ -8,6 +26,9 @@ class ITag:
 
     def text(self):
         raise NotImplmentedError()
+
+    def __str__(self):
+        return f'{ str(type(self)) } "{ str(self.text()) }"'
 
 
 class TagLiteral(ITag):
@@ -45,6 +66,7 @@ class TagTranslation(translate.ITranslation):
 
     def translate_tag(self, tag):
         """Takes an ITag or str and returns an ITag or str"""
+        info("translate_tag", tag)
 
         if type(tag) == str:
             return self.underlying_translation.translate(tag)
@@ -52,12 +74,13 @@ class TagTranslation(translate.ITranslation):
         text = tag.text()
         children = tag.children()
 
-        if (
-            len(text) > self.MAX_SEQUENCE_LENGTH
-            or len(list(filter(lambda x: isinstance(x, Tag), children))) > 0
-        ):
+        composite_tag_children = (
+            children is not None
+            and len(list(filter(lambda x: isinstance(x, Tag), children))) > 0
+        )
+        if len(text) > self.MAX_SEQUENCE_LENGTH or composite_tag_children:
             # Translate children seperatly
-            return Tag([translate_tag(self, child) for child in children])
+            return Tag([self.translate_tag(child) for child in children])
 
         translated_text = self.underlying_translation.translate(text)
 
@@ -70,20 +93,24 @@ class TagTranslation(translate.ITranslation):
 
         injection_tags = []
         for child in children:
-            print(child)
-            if isinstance(tag, TagLiteral):
-                print("PJDEBUG")
+            if isinstance(child, TagLiteral):
                 injection_tag = InjectionTag(
                     self.underlying_translation.translate(child.text())
                 )
                 injection_tags.append(injection_tag)
 
         for injection_tag in injection_tags:
-            injection_index = translated_text.find(child_translation)
+            injection_index = translated_text.find(injection_tag.text)
             if injection_index != -1:
                 injection_tag.injection_index = injection_index
             else:
-                return Tag([translate_tag(self, child) for child in children])
+                info(
+                    "translate_tag",
+                    "injection_tag.text not found",
+                    translated_text,
+                    injection_tag.text,
+                )
+                return Tag([self.translate_tag(child) for child in children])
 
         # TODO check for overlap
 
