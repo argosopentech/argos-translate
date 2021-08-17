19b9e7f84206377cd604859a6a044bcac12e52ee
YOGESHWARAN R <yogeshin247@gmail.com>
2021-08-17 13:05:58 +0530

    add tab completion for argostranslate

---
+++ tab_completions/completion.bash
@@ -0,0 +1,26 @@
+#/usr/bin/env bash
+
+# Tab Completion for argostranslate
+_argostranslate_completions ()  
+{
+  local cur
+
+  COMPREPLY=()
+  cur=${COMP_WORDS[COMP_CWORD]}
+
+  COMPREPLY=( $( compgen -W '--from-lang --to-lang --help' -- $cur ) )
+
+  case "$cur" in
+    -*)
+    COMPREPLY=( $( compgen -W '--from-lang --to-lang --help' -- $cur ) );;
+
+    ?)
+    COMPREPLY=( $( compgen -W 'en ar zh nl fr de hi id ga it ja ko pl pt ru es tr uk vi' -- $cur ) );;
+
+  esac
+
+  return 0
+}
+
+
+complete -F _argostranslate_completions argos-translate argos-translate-cli
