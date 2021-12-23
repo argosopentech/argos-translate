28ca44e7e30926ce5c869b5551502b997249f95a
P.J. Finlay <pjappdevelopment@gmail.com>
2021-12-22 18:09:17 -0600

    Removed PyQt thread

--- argostranslate/utils.py
+++ argostranslate/utils.py
@@ -1,7 +1,5 @@
 import logging
 
-from PyQt5.QtCore import *
-
 from argostranslate import settings
 
 logging.basicConfig(level=logging.DEBUG if settings.debug else logging.INFO)
@@ -15,16 +13,3 @@ def error(*argv):
     logging.error(str(argv))
 
 
-class WorkerThread(QThread):
-    """Runs a bound function on a thread"""
-
-    def __init__(self, bound_worker_function):
-        """Args:
-        bound_worker_function (functools.partial)
-        """
-        super().__init__()
-        self.bound_worker_function = bound_worker_function
-        self.finished.connect(self.deleteLater)
-
-    def run(self):
-        self.bound_worker_function()
