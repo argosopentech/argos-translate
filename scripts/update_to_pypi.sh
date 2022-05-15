292cfe99989235f422d0f34aad24e564c1bc9f83
P.J. Finlay <pjappdevelopment@gmail.com>
2022-05-15 12:58:11 -0500

    Add git tag doc

--- scripts/update_to_pypi.sh
+++ scripts/update_to_pypi.sh
@@ -4,6 +4,9 @@ exit 1
 
 # Make sure to update the version number in setup.py
 
+# Git tag version number
+# git tag -a v1.0.0
+
 # Run from root of project
 rm -rf build dist
 python setup.py sdist bdist_wheel
