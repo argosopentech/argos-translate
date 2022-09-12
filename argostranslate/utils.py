c82c21db63a16591e6852b78a7ce154c58b22cf1
P.J. Finlay <pjappdevelopment@gmail.com>
2022-09-12 08:11:39 -0500

    Print logs to standard output in debug mode

--- argostranslate/utils.py
+++ argostranslate/utils.py
@@ -1,9 +1,15 @@
 import logging
+import sys
 
 from argostranslate import settings
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG if settings.debug else logging.INFO)
+
+# Print to standard output if in debug mode
+if settings.debug:
+    std_out_stream_handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(std_out_stream_handler)
 
 
 def info(*argv):
