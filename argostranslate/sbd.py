4e75769423a4366dd3ffda123dc2e649fd0fd2cf
dingedi <contact@dingedi.com>
2022-10-12 11:28:22 +0200

    add types for sdb

--- argostranslate/sbd.py
+++ argostranslate/sbd.py
@@ -1,7 +1,9 @@
 from difflib import SequenceMatcher
 
-from argostranslate import package, settings
-from argostranslate.utils import info, error
+from argostranslate import package
+from argostranslate.package import Package
+from argostranslate.translate import ITranslation
+from argostranslate.utils import info
 
 fewshot_prompt = """<detect-sentence-boundaries> I walked down to the river. Then I went to the
 I walked down to the river. <sentence-boundary>
@@ -19,7 +21,7 @@ SENTENCE_BOUNDARY_TOKEN = "<sentence-boundary>"
 FEWSHOT_BOUNDARY_TOKEN = "-" * 10
 
 
-def get_sbd_package():
+def get_sbd_package() -> Package | None:
     packages = package.get_installed_packages()
     for pkg in packages:
         if pkg.type == "sbd":
@@ -27,14 +29,14 @@ def get_sbd_package():
     return None
 
 
-def generate_fewshot_sbd_prompt(input_text, sentence_guess_length=150):
+def generate_fewshot_sbd_prompt(input_text: str, sentence_guess_length: int = 150) -> str:
     sentence_guess = input_text[:sentence_guess_length]
     to_return = fewshot_prompt + "<detect-sentence-boundaries> " + sentence_guess
     info("generate_fewshot_sbd_prompt", to_return)
     return to_return
 
 
-def parse_fewshot_response(response_text):
+def parse_fewshot_response(response_text: str) -> str | None:
     response = response_text.split(FEWSHOT_BOUNDARY_TOKEN)
     info("parse_fewshot_response", response)
     if len(response) < 2:
@@ -45,7 +47,7 @@ def parse_fewshot_response(response_text):
     return response[-1]
 
 
-def process_seq2seq_sbd(input_text, sbd_translated_guess):
+def process_seq2seq_sbd(input_text: str, sbd_translated_guess: str) -> int:
     sbd_translated_guess_index = sbd_translated_guess.find(SENTENCE_BOUNDARY_TOKEN)
     if sbd_translated_guess_index != -1:
         sbd_translated_guess = sbd_translated_guess[:sbd_translated_guess_index]
@@ -65,16 +67,16 @@ def process_seq2seq_sbd(input_text, sbd_translated_guess):
         return -1
 
 
-def detect_sentence(input_text, sbd_translation, sentence_guess_length=150):
+def detect_sentence(input_text: str, sbd_translation: ITranslation, sentence_guess_length: int = 150) -> int:
     """Given input text, return the index after the end of the first sentence.
 
     Args:
-        input_text (str): The text to detect the first sentence of.
-        sbd_translation (translate.ITranslation): An ITranslation for detecting sentences.
-        sentence_guess_length (int): Estimated number of chars > than most sentences.
+        input_text: The text to detect the first sentence of.
+        sbd_translation: An ITranslation for detecting sentences.
+        sentence_guess_length: Estimated number of chars > than most sentences.
 
     Returns:
-        int: The index of the character after the end of the sentence.
+        The index of the character after the end of the sentence.
                 -1 if not found.
     """
     # TODO: Cache
