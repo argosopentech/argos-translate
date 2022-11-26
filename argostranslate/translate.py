b71eecfaae27f11a4e81680d3345c62ad0d4837a
P.J. Finlay <pjappdevelopment@gmail.com>
2022-11-26 14:30:03 -0600

    Bug fix
    - https://github.com/argosopentech/argos-translate/issues/298

--- argostranslate/translate.py
+++ argostranslate/translate.py
@@ -610,8 +610,7 @@ def get_language_from_code(code: str) -> Language | None:
     Returns:
         The language object
     """
-    return next(list(filter(lambda x: x.code == code, get_installed_languages())), None)
-    
+    return next(filter(lambda x: x.code == code, get_installed_languages()), None)
 
 
 def get_translation_from_codes(from_code: str, to_code: str) -> ITranslation:
