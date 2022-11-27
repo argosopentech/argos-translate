b67af34a4b57656c0d4093a95f04be7423b7f1f3
P.J. Finlay <pjappdevelopment@gmail.com>
2022-11-27 15:22:13 -0600

    Add argospm search test

--- tests/unit-tests/mock_argostranslate_package.py
+++ tests/unit-tests/mock_argostranslate_package.py
@@ -1,16 +1,33 @@
 import pathlib
 
 
-class Package:
+class IPackage:
     def __str__(self) -> str:
         return "Mock Package"
 
     def __repr__(self) -> str:
         return "MockPackage"
 
+    code: str = "mockpackage"
 
-mock_packages = [Package() for i in range(3)]
+
+class AvailablePackage(IPackage):
+    pass
 
 
-def get_installed_packages(packages_dir: pathlib.Path | None = None) -> list[Package]:
-    return mock_packages
+class Package(IPackage):
+    pass
+
+
+mock_available_packages = [AvailablePackage()] * 3
+
+
+def get_available_packages() -> list[AvailablePackage]:
+    return mock_available_packages
+
+
+mock_installed_packages = [Package() for i in range(3)]
+
+
+def get_installed_packages(packages_dir: pathlib.Path | None = None) -> list[IPackage]:
+    return mock_installed_packages
--- tests/unit-tests/test_argospm.py
+++ tests/unit-tests/test_argospm.py
@@ -15,3 +15,15 @@ def test_argospm_list():
         argostranslate.argospm.list_packages(None)
         expected_stdout = "MockPackage : Mock Package\n" * 3
         assert mock_stdout.getvalue() == expected_stdout
+
+@unittest.mock.patch(
+    "argostranslate.package.get_available_packages",
+    mock_argostranslate_package.get_available_packages,
+)
+def test_argospm_search_packages():
+    with unittest.mock.patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+        class Args:
+            query: str = "mockpackage"
+        argostranslate.argospm.search_packages(Args())
+        expected_stdout = "MockPackage - Mock Package\n" * 3
+        assert mock_stdout.getvalue() == expected_stdout
