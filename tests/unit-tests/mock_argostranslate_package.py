08000dd9258f1d72606aaae86001e42f20838d50
P.J. Finlay <pjappdevelopment@gmail.com>
2022-11-27 14:50:59 -0600

    Formatting

--- tests/unit-tests/mock_argostranslate_package.py
+++ tests/unit-tests/mock_argostranslate_package.py
@@ -1,9 +1,12 @@
 import pathlib
 
+
 class Package:
     pass
 
+
 mock_packages = [Package() for i in range(3)]
+
 
 def get_installed_packages(packages_dir: pathlib.Path | None = None) -> list[Package]:
     return mock_packages
--- tests/unit-tests/test_argospm.py
+++ tests/unit-tests/test_argospm.py
@@ -2,8 +2,12 @@ import unittest.mock
 
 import argostranslate.argospm
 
-import mock_argostranslate_package 
+import mock_argostranslate_package
 
-@unittest.mock.patch("argostranslate.package.get_installed_packages", mock_argostranslate_package.get_installed_packages)
+
+@unittest.mock.patch(
+    "argostranslate.package.get_installed_packages",
+    mock_argostranslate_package.get_installed_packages,
+)
 def test_argospm_list():
     argostranslate.argospm.list_packages(None)
