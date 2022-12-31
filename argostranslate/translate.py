ce5a3bc7af06cafbbd3aac691dfc173a69c2953a
P.J. Finlay <pjappdevelopment@gmail.com>
2022-12-31 17:56:07 -0600

    Expand docstring

--- argostranslate/translate.py
+++ argostranslate/translate.py
@@ -20,7 +20,7 @@ class Hypothesis:
 
     Attributes:
         value: The hypothetical translation value
-        score: The score representing the quality of the translation
+        score: The score representing the quality of the translation. Higher scores represent higher quality translations.
     """
 
     value: str
